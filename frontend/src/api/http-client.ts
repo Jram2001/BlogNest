e045490df4166799927b55c6e3f1e9d892b20467
Jayaram <99004590+Jram2001@users.noreply.github.com>
2025-08-03 21:02:09 +0530

    Update http-client.ts

--- frontend/src/api/http-client.ts
+++ frontend/src/api/http-client.ts
@@ -1,7 +1,7 @@
 import Axios, { AxiosError, type AxiosResponse, type InternalAxiosRequestConfig } from "axios";
 
 const apiClient = Axios.create({
-    baseURL: 'https://blognest-jm20.onrender.com',
+    baseURL: 'https://blognest-jm20.onrender.com/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
